543db30d8de22a7f14efdf05bc6ec7f8bee1c43c
Nuno Santos <nsantos.pessoal@gmail.com>
2018-05-21 17:01:57 +0100

    removed comments

--- Client/src/components/GDB.js
+++ Client/src/components/GDB.js
@@ -6,11 +6,7 @@ export default class GDB extends Component {
     const sLen = gdb.status.length
 
     console.log(`TAMANHO: ${sLen}`)
-    /*
-    customer
-    createdBy
-    creationDate
-    */
+    
     return (
       <div>
         { gdb.gdbno } - { gdb.customer } - { gdb.createdBy } - { gdb.creationDate } - { gdb.status[sLen - 1].statusType}
