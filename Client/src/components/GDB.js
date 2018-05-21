657c84ee970825d782785f7ee5901e49d145b3d3
Nuno Santos <nsantos.pessoal@gmail.com>
2018-05-21 16:40:38 +0100

    Further apollo-react testing

---
+++ Client/src/components/GDB.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class GDB extends Component {
+  render () {
+    const { gdb } = this.props
+    const sLen = gdb.status.length
+
+    console.log(`TAMANHO: ${sLen}`)
+    /*
+    customer
+    createdBy
+    creationDate
+    */
+    return (
+      <div>
+        { gdb.gdbno } - { gdb.customer } - { gdb.createdBy } - { gdb.creationDate } <br />
+        { gdb.status[sLen - 1].statusType}
+      </div>
+    )
+  }
+}
--- Client/src/components/Gdbs.js
+++ Client/src/components/Gdbs.js
@@ -2,20 +2,32 @@ import React from 'react'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
+import GDB from './GDB'
+
 const Q = gql`
 query GDBS {
   GDBS {
     gdbno
+    customer
+    createdBy
+    creationDate
+    status {
+      statusType
+      statusDate
+      statusDesc
+    }
   }
 }
 `
 
 const Gdbs = () => (
   <Query query={Q}>
-  {({ data, loading }) => {
+  {({ data, loading, error }) => {
     if (loading) {
       console.log('LOADING')
       return (<h1>LOADING</h1>)
+    } else if (error) {
+      return (<h1>ERROR { error }</h1>)
     } else {
       if (data) {
         console.log('GDBS')
@@ -23,6 +35,7 @@ const Gdbs = () => (
         return (
           <div>
             GDBS - { data.GDBS.length }
+            { data.GDBS.map(g => <GDB key={g.gdbno} gdb={g}/>)}
           </div>
         )
       }
